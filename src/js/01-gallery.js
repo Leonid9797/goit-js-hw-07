4488917357e5592c28f250c8f842e63a36d401b5
Leonid9797 <123494748+Leonid9797@users.noreply.github.com>
2023-06-05 14:34:50 +0200

    up

---
+++ src/js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryConteiner = document.querySelector(`.gallery`);
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryConteiner.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+galleryConteiner.addEventListener("click", onGalleryConteinerClick);
+
+function createGalleryCardsMarkup(alleryItems) {
+  return galleryItems
+    .map(({ preview, original, description } = {}) => {
+      return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryConteinerClick(evt) {
+  evt.preventDefault();
+
+  const isGalleryEl = evt.target.classList.contains("gallery__image");
+
+  if (!isGalleryEl) {
+    return;
+  }
+  const original = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${original}" width="800" height="600">
+`);
+
+  instance.show();
+}
---
+++ src/js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+const galleryConteiner = document.querySelector(`.gallery`);
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryConteiner.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+galleryConteiner.addEventListener("click", onGalleryConteinerClick);
+
+function createGalleryCardsMarkup(alleryItems) {
+  return galleryItems
+    .map(({ preview, original, description } = {}) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryConteinerClick(evt) {
+  evt.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
